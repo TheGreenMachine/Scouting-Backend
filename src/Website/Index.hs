82fcf4ee86db67337eff42735149384958256193
Danny Gratzer <danny.gratzer@gmail.com>
2013-02-25 16:27:02 -0600

    Various touchups to the template

--- src/Website/Index.hs
+++ src/Website/Index.hs
@@ -3,6 +3,7 @@ module Website.Index where
 import Control.Monad
 import Data.List
 import Data.Ord
+import Data.Function
 import Text.Blaze.Html5 hiding (map)
 import Text.Blaze.Html5.Attributes
 import Text.Blaze.Renderer.String
@@ -11,29 +12,35 @@ import qualified Text.Blaze.Html5.Attributes as A
 import GameData
 genIndex :: [TeamInfo] -> IO ()
 genIndex teams = do
-  let orderedList = sortBy (comparing snd) $ zip teams (map average teams)
+  let orderedList = sortBy (rcompare `on` snd)$ zip teams (map average teams)
   let html = template orderedList
   writeFile "site/index.html" $ renderHtml html
   where
     average team =  (sum . map ($team) $ [auto, main, climb]) / 3
+    rcompare a b = case compare a b of
+      LT -> GT
+      GT -> LT
+      EQ -> EQ
 template :: [(TeamInfo, Double)] -> Html
 template list = docTypeHtml $ do
   H.head $ do
     H.title "Home"
   body $ do
     h1 "Main Team Index"
-    table ! customAttribute "border" "1" $ do
-      th "Name"
-      th "Average"
-      th "Autonomous"
-      th "Teleop"
-      th "Climbing"
+    table ! customAttribute "border" "1"
+          ! customAttribute "cellpadding" "10" $ do
+      th $ h2 "Rank"
+      th $ h2 "Name"
+      th $ h2 "Overall"
+      th $ h2 "Autonomous"
+      th $ h2 "Teleop"
+      th $ h2 "Climbing"
       mapM_
-        (\(info, avg) -> tr $ do
-                         td (a ! href (toValue $ show (number info) ++ ".html") $ toHtml $ number info)
+        (\((info, avg), rank) -> tr $ do
+                         td . toHtml $ rank
+                         td (a ! href (toValue $ show (number info) ++ ".html") $ h3 . toHtml $ number info)
                          td . toHtml $ (truncate avg :: Int)
                          td . toHtml $ (truncate $ auto  info :: Int)
                          td . toHtml $ (truncate $ main  info :: Int)
                          td . toHtml $ (truncate $ climb info :: Int))
-        list
-  where showTuple (a, b) = show a ++ " : " ++ show b
+        (zip list ([1..] :: [Int]))
