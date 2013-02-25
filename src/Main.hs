e63d0f21d69253f53bed96a5e07cde3a1d209f92
Danny Gratzer <danny.gratzer@gmail.com>
2013-02-24 23:14:53 -0600

    Moved the src files into src/

---
+++ src/Main.hs
@@ -0,0 +1,14 @@
+#!/usr/bin/runhaskell
+module Main where
+import GameLoader
+import Data.String.Utils
+import Website
+main = do
+  putStrLn "Welcome to the HSB.\nPlease standby..."
+  putStrLn "Loading Team Info"
+  teamInfos <- loadTeams
+  writeFile "teamdata.csv" . join "\n" . map show $ teamInfos --Write the main team info out for AO
+  putStrLn "Finished loading team info. CSV was written to 'teamdata.csv'"
+  --Build Website--
+  putStrLn "Building the Static Website..."
+  generateSite teamInfos
