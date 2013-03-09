7b60641365f42472bb6f1368085ce257dfb7b4a1
Danny Gratzer <danny.gratzer@gmail.com>
2013-03-08 20:44:02 -0600

    Ready for presi

--- src/Website/Template.hs
+++ src/Website/Template.hs
@@ -5,7 +5,7 @@ module Website.Template (
   PartialToken
   ) where
 import Text.Blaze.Html5 hiding (map)
-import Text.Blaze.Html5.Attributes
+import Text.Blaze.Html5.Attributes hiding (id)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import GameData
@@ -27,17 +27,18 @@ type PartialToken = String -> TemplateToken
 wrapTemplate :: TemplateToken  -- | The token containing all data for the menus
                 -> Html        -- | The HTML to be wrapped.
                 -> Html        -- | The wrapped HTML
-wrapTemplate token page = docTypeHtml $ do
-  link ! rel "stylesheet" ! type_ "text/css" ! href (toValue $ path token ++ "screen.css")
-  ul ! A.id "nav-left" $ mapM_ (\team -> li
-                       (a ! href (toValue $ path token ++ show (number team) ++ ".html") $ toHtml $ number team)) $
-    teamList token
-  ul ! A.id "nav-right" $ mapM_ (\(MatchInfo (a1, a2)) -> li
-                                       (a ! href (toValue $ path token ++ "matches/" ++ round a1 ++ ".html") $ toHtml (round a1))) $
-    matchList token
-  ul ! A.id "nav-top" $ do
-    li $ a ! href (toValue $ path token ++ "index.html") $ " Average "
-    li $ a ! href (toValue $ path token ++ "auto.html")  $ " Autonomous "
-    li $ a ! href (toValue $ path token ++ "main.html")  $ " Teleop "
-    li $ a ! href (toValue $ path token ++ "climb.html") $ " Climbing "
-  div ! customAttribute "class" "main-page" $ page
+wrapTemplate _ = id
+--wrapTemplate token page = docTypeHtml $ do
+--  link ! rel "stylesheet" ! type_ "text/css" ! href (toValue $ path token ++ "screen.css")
+--  ul ! A.id "nav-left" $ mapM_ (\team -> li
+--                       (a ! href (toValue $ path token ++ show (number team) ++ ".html") $ toHtml $ number team)) $
+--    teamList token
+--  ul ! A.id "nav-right" $ mapM_ (\(MatchInfo (a1, a2)) -> li
+--                                       (a ! href (toValue $ path token ++ "matches/" ++ round a1 ++ ".html") $ toHtml (round a1))) $
+--    matchList token
+--  ul ! A.id "nav-top" $ do
+--    li $ a ! href (toValue $ path token ++ "index.html") $ " Average "
+--    li $ a ! href (toValue $ path token ++ "auto.html")  $ " Autonomous "
+--    li $ a ! href (toValue $ path token ++ "main.html")  $ " Teleop "
+--    li $ a ! href (toValue $ path token ++ "climb.html") $ " Climbing "
+--  div ! customAttribute "class" "main-page" $ page
